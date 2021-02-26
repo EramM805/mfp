e49d4ad1aca8f7dac767a296de875e8be426c8e2
Eram M <eram.manasia@gmail.com>
2021-02-26 13:49:05 -0500

    Should fix build issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
     return <div>
-        <h1>Hi There! Container is here</h1>
+        <h1>Hi There! Container </h1>
         <hr />
         <MarketingApp />
     </div>
