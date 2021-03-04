4c1a6711fe14686a32dc5963a194c450c867a6fa
Eram M <eram.manasia@gmail.com>
2021-03-04 15:01:11 -0500

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
     return <div>
-        <h1>Hi There! Container should be working....</h1>
+        <h1>Hi There! Container should be working...</h1>
         <hr />
         <MarketingApp />
     </div>
